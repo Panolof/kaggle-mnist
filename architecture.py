c995a4e0a757b4c98ea96b235dc0edeacafb1f94
Panolof <pano.pouroullis@gmail.com>
2021-09-08 01:49:17 +0100

    Update to tensorflow imports

---
+++ architecture.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  8 00:38:39 2021
+
+@author: Pano
+"""
+from tensorflow import keras
+from tensorflow.keras import layers
+
+
+model = keras.Sequential([
+    layers.Dense()
+    ])
